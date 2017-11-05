f623a40de08b2d8ff15d3c9c8330738784e2edfe
rHongCN <rHongCN@QQ.com>
2017-11-05 12:18:51 +0800

    .

---
+++ include/NrScript/xml/NrXmlNode.h
@@ -0,0 +1,27 @@
+﻿#ifndef _NRSCRIPT_NRXMLNODE_H_
+#define _NRSCRIPT_NRXMLNODE_H_ 1
+
+/**
+ * xml节点
+ */
+class NRSCRIPT_API_VISUAL NrXmlNode {
+public:
+    /**
+     * 默认构造函数
+     */
+    NrXmlNode();
+
+    /**
+     * 虚拟析构函数
+     */
+    virtual ~NrXmlNode();
+
+private:
+    /**
+     * 桥接
+     */
+    class Impl;
+    Impl* impl {nullptr};
+};
+
+#endif //
---
+++ src/NrScript/xml/NrXmlNode.cpp
@@ -0,0 +1,15 @@
+#include "NrScript/base.h"
+#include "NrScript/xml/NrXmlNode.h"
+
+class NrXmlNode::Impl {
+public:
+
+};
+
+NrXmlNode::NrXmlNode() {
+
+}
+
+NrXmlNode::~NrXmlNode() {
+
+}
